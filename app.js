224008b9739a1bae4a2d50a81d36be831a36edf5
Phil <happy.melodist@gmail.com>
2021-12-31 01:47:38 -0600

    establish the initial position of the orange, randomize the velocities, and write the coin flip if statements

--- app.js
+++ app.js
@@ -1,5 +1,74 @@
+// const canvasEl = document.querySelector("canvas");
+// const canvasContext = canvasEl.getContext("2d");
+
+// const FPS = 60;
+// let radius = 50;
+// let xP, yP;
+// let xV, yV;
+
+// xP = canvasEl.width / 2;
+// yP = canvasEl.height / 2;
+
+// xV = Math.floor(Math.random() * 201 + 99) / FPS;
+// yV = Math.floor(Math.random() * 201 + 99) / FPS;
+
+// // coin toss situation
+// if (Math.floor(Math.random() * 2) === 0) {
+//   xV = -xV;
+// }
+
+// if (Math.floor(Math.random() * 2) === 0) {
+//   yV = -yV;
+// }
+
+// // The Game Loop
+// function runGame() {
+//   //----------------- moving the ball from its current position
+//   xP += xV;
+//   yP += yV;
+
+//   //--------------- clearing the canvas
+//   canvasContext.clearRect(0, 0, canvasEl.width, canvasEl.height);
+
+//   //------------------- Collision Detection
+//   // bottom boundary
+//   if (yV > 0 && yP >= canvasEl.height - radius) {
+//     yV = -yV;
+//   }
+
+//   // right boundary
+//   if (xV > 0 && xP > canvasEl.width - radius) {
+//     xV = -xV;
+//   }
+
+//   // top boundary
+//   if (yV < 0 && yP <= radius) {
+//     yV = -yV;
+//   }
+
+//   // left boundary
+//   if (xV < 0 && xP <= radius) {
+//     xV = -xV;
+//   }
+
+//   // ----------------Drawing the ball
+//   canvasContext.beginPath();
+//   canvasContext.fillStyle = "orange";
+//   canvasContext.arc(xP, yP, radius, 0, Math.PI * 2);
+//   canvasContext.fill();
+// }
+
+// // set interval
+// setInterval(runGame, 1000 / FPS);
+
+// get busy living or get busy dying
+
+/*
+my code below
+*/
+
 const canvasEl = document.querySelector("canvas");
-const canvasContext = canvasEl.getContext("2d");
+const canvasCTX = canvasEl.getContext("2d");
 
 const FPS = 60;
 let radius = 50;
@@ -7,12 +76,13 @@ let xP, yP;
 let xV, yV;
 
 xP = canvasEl.width / 2;
-yP = canvasEl.height / 2;
+yp = canvasEl.height / 2;
 
 xV = Math.floor(Math.random() * 201 + 99) / FPS;
 yV = Math.floor(Math.random() * 201 + 99) / FPS;
 
 // coin toss situation
+
 if (Math.floor(Math.random() * 2) === 0) {
   xV = -xV;
 }
@@ -20,43 +90,3 @@ if (Math.floor(Math.random() * 2) === 0) {
 if (Math.floor(Math.random() * 2) === 0) {
   yV = -yV;
 }
-
-// The Game Loop
-function runGame() {
-  //----------------- moving the ball from its current position
-  xP += xV;
-  yP += yV;
-
-  //--------------- clearing the canvas
-  canvasContext.clearRect(0, 0, canvasEl.width, canvasEl.height);
-
-  //------------------- Collision Detection
-  // bottom boundary
-  if (yV > 0 && yP >= canvasEl.height - radius) {
-    yV = -yV;
-  }
-
-  // right boundary
-  if (xV > 0 && xP > canvasEl.width - radius) {
-    xV = -xV;
-  }
-
-  // top boundary
-  if (yV < 0 && yP <= radius) {
-    yV = -yV;
-  }
-
-  // left boundary
-  if (xV < 0 && xP <= radius) {
-    xV = -xV;
-  }
-
-  // ----------------Drawing the ball
-  canvasContext.beginPath();
-  canvasContext.fillStyle = "orange";
-  canvasContext.arc(xP, yP, radius, 0, Math.PI * 2);
-  canvasContext.fill();
-}
-
-// set interval
-setInterval(runGame, 1000 / FPS);
